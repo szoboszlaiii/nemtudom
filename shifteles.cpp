1557f8ed08aa527b49cc9343d30c674ac8a97aa0
szoboszlaiii <55443389+szoboszlaiii@users.noreply.github.com>
2019-09-20 15:03:14 +0200

    Add files via upload

---
+++ shifteles.cpp
@@ -0,0 +1,17 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+  int i=1;
+  int db=0;
+  
+  while (i!=0){
+      i=i<<1;
+      db++;
+      
+      }
+  cout<<db<<"\n";
+  
+  return 0;
+   
+}
