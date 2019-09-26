1c5d1684dc581d07ea986f9ce5c9ab6471817ccf
szoboszlaiii <55443389+szoboszlaiii@users.noreply.github.com>
2019-09-26 20:02:23 +0200

    Add files via upload

---
+++ drill5.cpp
@@ -0,0 +1,31 @@
+#include "std_lib_facilities.h"
+
+int main(){
+/*cout<<"Success!\n";
+cout<<"Success!\n";
+cout<<"Success" << "!\n";
+cout<<"Success!"<<'\n';
+int res =7; vector<int> v(10); v[5] = res; cout << "Success!\n";
+vector <int> v(10); v[5] = 7; if (v[5]=7) cout << "Success!\n";
+bool cond = true; if (cond) cout << "Success!\n"; else cout << "Fail!\n";
+bool c = true; if (c) cout << "Success!\n"; else cout << "Fail!\n";
+string s = "ape"; bool c =true; if (c) cout << "Success!\n";
+string s = "ape"; if (s=="ape") cout << "Success!\n";
+string s = "fool"; if (s=="fool") cout << "Success!\n";
+string s = "ape"; if (s=="ape") cout << "Success!\n";
+vector<char> v(5); for (int i=0; i<v.size(); i++) cout << "Success!\n";
+vector<char> v(5); for (int i=0; i<=v.size(); i++) cout << "Success!\n";
+string s = "Success!"; for (int i=0; i<8; i++) cout << s[i];
+if (true) cout << "Success!\n"; else cout << "Fail!\n";
+int x = 2000; int c = x; if (c==2000) cout << "Success!\n";
+string s = "Success!\n"; for (int i=0; i<8;i++) cout << s[i];
+vector <int> v(5); for (int i=0; i<=v.size(); i++) cout << "Success!\n";
+int i=0; int j = 9; while (i<10){ i++; if (j<i) cout << "Success!\n";}
+int x = 2; double d =4.5; if (d==2*x+0.5) cout << "Success!\n";
+string s = "Success!"; for (int i=0; i<=7; i++) cout << s[i];
+int i=0; int j=9; while (i<10){ i++; if (j<i) cout << "Success!\n";} */
+int x = 4; double d = 5/x-2; if (d==(x-2+0.5)) cout << "Success!\n";
+cout <<"Success!\n";
+
+return 0;
+}
