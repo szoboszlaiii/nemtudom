e1742f05f96c68e6ce91f022fb30ed5db8f5ae7b
szoboszlaiii <55443389+szoboszlaiii@users.noreply.github.com>
2019-11-04 12:25:53 +0100

    Add files via upload

---
+++ drill8/my.cpp
@@ -0,0 +1,16 @@
+#include "my.h"
+#include "std_lib_facilities.h"
+
+int foo;
+
+void print_foo(){
+	cout<<foo<<'\n';
+}
+
+void print(int i){
+	cout<<i<<'\n';
+}
+
+int main(){
+return 0;
+}
---
+++ drill8/use.cpp
@@ -0,0 +1,13 @@
+#include "my.h"
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	foo=7;
+	print_foo();
+	print(99);
+	char cc;
+	cin >> cc;
+	return 0;
+}
